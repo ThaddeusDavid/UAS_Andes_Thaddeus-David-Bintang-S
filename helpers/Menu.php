676dfdf16ec6946b6171290e6794c088982b342b
ThaddeusDavid <119637648+ThaddeusDavid@users.noreply.github.com>
2022-12-16 14:31:00 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'levels', 
+			'label' => 'Levels', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'masakans', 
+			'label' => 'Masakans', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'mejas', 
+			'label' => 'Mejas', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'order_details', 
+			'label' => 'Order Details', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'orders', 
+			'label' => 'Orders', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'transactions', 
+			'label' => 'Transactions', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+		
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'levels', 
+			'label' => 'Levels', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'masakans', 
+			'label' => 'Masakans', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'mejas', 
+			'label' => 'Mejas', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'order_details', 
+			'label' => 'Order Details', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'orders', 
+			'label' => 'Orders', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'transactions', 
+			'label' => 'Transactions', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
